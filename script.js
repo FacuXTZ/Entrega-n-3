60a3aa001402b913518879b9dec333dc8364744f
Facundo Molina <solojuegos102010@outlook.com>
2025-01-30 21:49:46 -0300

    añadido todo el full responsive

---
+++ script.js
@@ -0,0 +1,5 @@
+bars = document.querySelector(".bars");
+bars.onclick = function() {                            // esto es para el full responsive 
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active")
+}
